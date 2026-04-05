a1dc2d76e41ed016d6b60e17ec17f784164c32a3
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-04-06 00:23:30 +0100

    Day 1 (simple solution)

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,11 +1,61 @@
-module Day1 where
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+{-# LANGUAGE DerivingVia #-}
 
-data Answer = Answer deriving (Eq, Show)
+module Day1 (parse, solve, program, solve1, solve2, winner, topWinners, Elf (Elf), Calories (Calories), Solution (Solution)) where
+
+import Control.Monad.Combinators.NonEmpty (sepEndBy1)
+import Data.Bifunctor (first)
+import Data.Foldable1 (Foldable1 (fold1, foldMap1), maximumBy)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as N
+import Data.Monoid (Sum (Sum))
+import Data.Ord (Down (Down), comparing)
+import qualified Data.Text as T
+import Data.Void (Void)
+import Text.Megaparsec (MonadParsec (eof), Parsec, errorBundlePretty, runParser)
+import Text.Megaparsec.Char (newline)
+import Text.Megaparsec.Char.Lexer (decimal)
+
+data Solution = Solution {solution1 :: Calories, solution2 :: Calories} deriving (Eq, Show)
+
+newtype Calories = Calories Int
+  deriving (Eq, Ord, Show, Num) via Int
+  deriving (Semigroup) via (Sum Int)
+
+data Elf = Elf (NonEmpty Calories) deriving (Eq, Show)
+
+totalCalories :: Elf -> Calories
+totalCalories (Elf calories) = fold1 calories
+
+winner :: NonEmpty Elf -> Elf
+winner = maximumBy (comparing totalCalories)
+
+topWinners :: Int -> NonEmpty Elf -> NonEmpty Elf
+topWinners n = takeAtLeastOne n . N.sortOn (Down . totalCalories)
+ where
+  takeAtLeastOne m = (N.:|) <$> N.head <*> (take (max 0 (m - 1)) . N.tail)
+
+solve1 :: NonEmpty Elf -> Calories
+solve1 = totalCalories . winner
+
+solve2 :: NonEmpty Elf -> Calories
+solve2 = foldMap1 totalCalories . topWinners 3
 
 program :: T.Text -> IO ()
-program = print . logic
+program = print . solve
 
-logic :: T.Text -> Answer
-logic = const Answer
+solve :: T.Text -> Either String Solution
+solve = fmap (Solution <$> solve1 <*> solve2) . parse
+
+type Parser = Parsec Void T.Text
+
+caloriesParser :: Parser Calories
+caloriesParser = Calories <$> decimal
+
+elfParser :: Parser Elf
+elfParser = Elf <$> sepEndBy1 caloriesParser newline
+
+elfsParser :: Parser (NonEmpty Elf)
+elfsParser = sepEndBy1 elfParser newline <* eof
+
+parse :: T.Text -> Either String (NonEmpty Elf)
+parse = first errorBundlePretty . runParser elfsParser "Day 1 parsing"
